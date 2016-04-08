66328af4bb491112e081786249bc77c5e310a691
Tarik Merzouk <tarikmerzouk@yahoo.com>
2016-04-08 13:59:13 -0700

    initial committ

---
+++ NamesMain.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tarik
+ *
+ */
+public class NamesMain {
+
+	/**
+	 * call your name methods in main
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		tarikMerzouk();
+
+	}
+	
+	public static void tarikMerzouk() {
+		System.out.println(">~>~Tarik-illah the mike kilah~>~>");
+	}
+
+}
